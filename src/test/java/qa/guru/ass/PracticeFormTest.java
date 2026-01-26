75318dd140dcdf3d0d25177548658c65ecfae02f
SSAbramova <ssabramova@inno.tech.ru>
2026-01-26 21:15:24 +0300

    added base ui test

---
+++ src/test/java/qa/guru/ass/PracticeFormTest.java
@@ -0,0 +1,56 @@
+package qa.guru.ass;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+
+public class PracticeFormTest {
+
+    @BeforeAll
+    static void testPrecondition() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        $("#firstName").setValue("Svetlana");
+        $("#lastName").setValue("Abramova");
+        $("#userEmail").setValue("sabramova@abrams.com");
+        $("#genterWrapper").$(byText("Female")).click();
+        $("#userNumber").setValue("1234567890");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").$(byText("January")).click();
+        $(".react-datepicker__year-select").$(byText("1994")).click();
+        $(".react-datepicker__month").$(byText("10")).click();
+        $("#subjectsInput").setValue("Physics").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("check.PNG");
+        $("#currentAddress").setValue("Pushkin Street 1");
+        $("#state").click();
+        $(byText("NCR")).click();
+        $("#city").click();
+        $(byText("Delhi")).click();
+        $("#submit").click();
+
+        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text("Svetlana Abramova"));
+        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text("sabramova@abrams.com"));
+        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Female"));
+        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("1234567890"));
+        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("10 January,1994"));
+        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("Physics"));
+        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Sports"));
+        $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("check.PNG"));
+        $(".table-responsive").$(byText("Address")).parent().shouldHave(text("Pushkin Street 1"));
+        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("NCR Delhi"));
+        $("#closeLargeModal").click();
+
+    }
+}
